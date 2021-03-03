be9a0fb79921d0877fb6c6c8b2246e62d0c9a884
evaspirovska <67806019+evaspirovska@users.noreply.github.com>
2021-03-03 19:46:29 +0100

    Added new methods

--- WebProgramming_Project/src/main/java/onlineshopapp/fashionstore/web/controller/ClothesController.java
+++ WebProgramming_Project/src/main/java/onlineshopapp/fashionstore/web/controller/ClothesController.java
@@ -1,7 +1,35 @@
 package onlineshopapp.fashionstore.web.controller;
 
+import onlineshopapp.fashionstore.model.Clothes;
+import onlineshopapp.fashionstore.service.ClothesService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/products")
 public class ClothesController {
+    private static ClothesService clothesService;
+
+    public ClothesController(ClothesService clothesService) {
+        this.clothesService = clothesService;
+    }
+
+    @GetMapping
+    public String getProductPage(@RequestParam(required = false) String error, Model model) {
+        if (error != null && !error.isEmpty()) {
+            model.addAttribute("hasError", true);
+            model.addAttribute("error", error);
+        }
+        List<Clothes> products = this.clothesService.findAll();
+        model.addAttribute("products", products);
+        model.addAttribute("bodyContent", "products");
+        return "products";
+    }
+
+
 }
