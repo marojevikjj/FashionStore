dc53a67a00a1e1ee875952efc63315c9bd12070d
EmilijaT <67805805+EmilijaT@users.noreply.github.com>
2021-03-15 02:58:02 +0100

    Navigation

--- WebProgramming_Project/src/main/java/onlineshopapp/fashionstore/web/controller/CartController.java
+++ WebProgramming_Project/src/main/java/onlineshopapp/fashionstore/web/controller/CartController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/orders")
-public class OrderController {
+@RequestMapping("/cart")
+public class CartController {
 
     @GetMapping
-    public String showOrderPage() {
-        return "orders";
+    public String showHomePage() {
+        return "cart";
     }
 }
