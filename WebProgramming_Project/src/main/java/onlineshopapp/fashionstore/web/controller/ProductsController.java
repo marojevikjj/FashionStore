6e9aeecacb9c9cd01c36047f40285701adead397
EmilijaT <67805805+EmilijaT@users.noreply.github.com>
2021-03-15 04:07:04 +0100

    product details page

--- WebProgramming_Project/src/main/java/onlineshopapp/fashionstore/web/controller/ProductsController.java
+++ WebProgramming_Project/src/main/java/onlineshopapp/fashionstore/web/controller/ProductsController.java
@@ -4,6 +4,7 @@ import onlineshopapp.fashionstore.service.ClothesService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -38,5 +39,11 @@ public class ProductsController {
             model.addAttribute("products", this.service.listProductsByNameAndCategory(nameSearch, categoryId));
         } */
         return "products";
+    }
+
+    @GetMapping("/{id}")
+    public String showDetails(@PathVariable Long id, Model model) {
+        model.addAttribute("product", this.clothesService.findById(id));
+        return "product-details";
     }
 }
