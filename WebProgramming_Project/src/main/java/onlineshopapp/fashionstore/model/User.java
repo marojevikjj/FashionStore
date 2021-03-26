fb865e7772785d51505acb37d30ec92cdad047ff
marojevikjj <jovana.marojevikj@gmail.com>
2021-03-26 10:22:51 +0100

    Spring security authorities fix

--- WebProgramming_Project/src/main/java/onlineshopapp/fashionstore/model/User.java
+++ WebProgramming_Project/src/main/java/onlineshopapp/fashionstore/model/User.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
 
 @Data
@@ -58,7 +59,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return Collections.singletonList(role);
     }
 
     @Override
