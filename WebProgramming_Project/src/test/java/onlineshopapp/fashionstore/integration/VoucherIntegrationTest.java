613177257858f51b0e7498cfd5fe5f919633968b
evaspirovska <67806019+evaspirovska@users.noreply.github.com>
2021-09-09 00:16:37 +0200

    added PostmanController tests

--- WebProgramming_Project/src/test/java/onlineshopapp/fashionstore/integration/VoucherIntegrationTest.java
+++ WebProgramming_Project/src/test/java/onlineshopapp/fashionstore/integration/VoucherIntegrationTest.java
@@ -1,14 +1,9 @@
 package onlineshopapp.fashionstore.integration;
 
-import onlineshopapp.fashionstore.model.enumerations.Role;
-import onlineshopapp.fashionstore.service.UserService;
-import onlineshopapp.fashionstore.service.VoucherService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -27,14 +22,6 @@ import org.springframework.web.context.WebApplicationContext;
 public class VoucherIntegrationTest {
 
     MockMvc mockMvc;
-
-    @Autowired
-    UserService userService;
-
-    @Autowired
-    VoucherService voucherService;
-
-    private static boolean dataInitialized = false;
 
     @BeforeEach
     public void setup(WebApplicationContext wac) {
